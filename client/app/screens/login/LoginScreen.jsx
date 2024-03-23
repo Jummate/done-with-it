c3585f643cbc33a70e10233b80f4dcf75f26ece0
Jummate <yakubjumat@gmail.com>
2024-03-23 16:28:21 +0100

    refactor: encapsulate validation field and error into a separate component

--- client/app/screens/login/LoginScreen.jsx
+++ client/app/screens/login/LoginScreen.jsx
@@ -5,10 +5,11 @@ import * as Yup from "yup";
 
 import Screen from "../../components/screen/Screen";
 import { styles } from "./styles";
-import AppTextInput from "../../components/app-textinput/AppTextInput";
+// import AppTextInput from "../../components/app-textinput/AppTextInput";
 import AppButton from "../../components/app-button/AppButton";
-import AppText from "../../components/app-text/AppText";
-import ErrorMessage from "../../components/ErrorMessage";
+// import AppText from "../../components/app-text/AppText";
+// import ErrorMessage from "../../components/ErrorMessage";
+import AppFormField from "../../components/AppFormField";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
@@ -28,41 +29,28 @@ function LoginScreen(props) {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
+        {({ handleSubmit }) => (
           <>
-            <AppTextInput
+            <AppFormField
               autoCapitalize="none"
               autoCorrect={false}
               icon="email"
               keyboardType="email-address"
-              onBlur={() => setFieldTouched("email")}
-              onChangeText={handleChange("email")}
+              name="email"
               placeholder="Email"
               textContentType="emailAddress"
             />
-            <ErrorMessage
-              error={errors.email}
-              visible={touched.email}
-            />
-            {/* {touched.email && <ErrorMessage error={errors.email} />} */}
-            {/* <AppText style={{ color: "red" }}>{errors.email}</AppText> */}
-            <AppTextInput
+
+            <AppFormField
               autoCapitalize="none"
               autoCorrect={false}
               icon="lock"
-              onBlur={() => setFieldTouched("password")}
-              onChangeText={handleChange("password")}
+              name="password"
               placeholder="Password"
               textContentType="password"
               secureTextEntry
             />
 
-            <ErrorMessage
-              error={errors.password}
-              visible={touched.password}
-            />
-            {/* {touched.password && <ErrorMessage error={errors.password} />} */}
-            {/* <AppText style={{ color: "red" }}>{errors.password}</AppText> */}
             <AppButton
               title="LOG IN"
               onPress={handleSubmit}
